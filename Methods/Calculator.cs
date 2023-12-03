fbc4ecaa30ede35770f6322249598114c0f1d4ec
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-04 00:36:32 +0300

    Add files via upload

---
+++ Methods/Calculator.cs
@@ -0,0 +1,20 @@
+﻿namespace Methods
+{
+
+    public partial class Program
+    {
+        public class Calculator
+        {
+            public int Add(params int[] numbers)
+            {
+                var sum = 0;
+                foreach (var number in numbers)
+                {
+                    sum += number;
+                }
+
+                return sum;
+            }
+        }
+    }
+}
