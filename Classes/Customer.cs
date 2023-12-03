931c91fb5b99d4739b1bc03b8a4277ca7f59b78f
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-04 00:35:50 +0300

    Add files via upload

---
+++ Classes/Customer.cs
@@ -0,0 +1,25 @@
+﻿namespace Classes
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+        public List<Order> Orders;
+        public Customer()
+        {
+            Orders = new List<Order>();
+        }
+
+        public Customer(int Id)
+            : this()
+        {
+            this.Id = Id;
+        }
+
+        public Customer(int id, string name)
+            : this(id)
+        {
+            this.Name = name;
+        }
+    }
+}
