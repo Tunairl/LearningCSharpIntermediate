9f96e5c81b5e7875f5eba9c9492456f8adb22ac3
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-09 22:53:42 +0300

    Add files via upload

---
+++ Classes/IntroductionToClasses/Constructors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+
+    internal class Constructors
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer(1, "Gülben");
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+
+            var order  = new Order();
+            customer.Orders.Add(order); 
+        }
+    }
+}
