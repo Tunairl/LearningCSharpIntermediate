80cf80f9f7e41e603ff6c1fa7aa0a72c25157f5e
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-10 03:12:11 +0300

    Add files via upload

---
+++ Classes/Properties/Person.cs
@@ -0,0 +1,28 @@
+﻿namespace Properties
+{
+    public class Person
+    {
+        //use the code snippet "prop" to create a auto-implemented property
+        //auto-implemented properties are should be at the top 
+        //calculated properties should be at the bottom
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public DateTime Birthdate { get; private set; }
+
+        public Person(DateTime birthdate)
+        {
+            Birthdate = birthdate;
+        }
+
+        public float Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - Birthdate;
+                var years = timeSpan.Days / 365f;
+
+                return years;
+            }
+        }
+    }
+}
---
+++ Classes/Properties/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person(new DateTime(2002, 09, 10));
+            Console.WriteLine(person.Age);
+        }
+    }
+}
