7520aebb7706bb89155d1675d998c4f55066788a
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-12 02:52:43 +0300

    Add files via upload

---
+++ Classes/Exercises/Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            Thread.Sleep(1000);
+            sw.Stop();
+            Console.WriteLine(sw.Stop());
+        }
+    }
+}
---
+++ Classes/Exercises/Stopwatch/Stopwatch/Stopwatch.cs
@@ -0,0 +1,33 @@
+﻿namespace Stopwatch
+{
+    public class Stopwatch
+    {
+        DateTime _startingPoint = DateTime.MinValue;
+
+        public void Start()
+        {
+            if (_startingPoint != DateTime.MinValue)
+            {
+                throw new InvalidOperationException("Cannot be started twice! fuck you");
+            }
+            _startingPoint = DateTime.Now;
+        }
+
+
+        public TimeSpan Stop()
+        {
+            if (_startingPoint == DateTime.MinValue)
+            {
+                throw new InvalidOperationException("Cannot be stopped without starting! fuck you");
+            }
+            var endingPoint = DateTime.Now;
+
+            var duration = endingPoint - _startingPoint;
+
+            _startingPoint = DateTime.MinValue;
+
+            return duration;
+        }
+
+    }
+}
