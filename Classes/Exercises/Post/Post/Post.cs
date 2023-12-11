4d9c8b110fa9bcbde645c80670739194b59e32de
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-12 02:53:44 +0300

    Add files via upload

---
+++ Classes/Exercises/Post/Post/Post.cs
@@ -0,0 +1,32 @@
+﻿namespace Post
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateOfCreation { get; private init; }
+
+        public int VoteCount { get; private set; }
+
+        public Post()
+        {
+        }
+
+        public Post(string title, string description)
+        {
+            this.Title = title;
+            this.Description = description;
+            this.DateOfCreation = DateTime.Now;
+        }
+
+        public void Upvote()
+        {
+            VoteCount += 1;
+        }
+        
+        public void Downvote()
+        {
+            VoteCount -= 1;
+        }
+    }
+}
---
+++ Classes/Exercises/Post/Post/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Post
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Post();
+            p1.Upvote();
+            p1.Upvote();
+            p1.Upvote();
+            p1.Upvote();
+            p1.Downvote();
+
+            Console.WriteLine(p1.VoteCount);
+
+            var p2 = new Post("StackOverflow", "This is my first StackOverflow post. Fuck you.");
+            p2.Upvote();
+            p2.Upvote();
+            p2.Upvote();
+        }
+    }
+}
