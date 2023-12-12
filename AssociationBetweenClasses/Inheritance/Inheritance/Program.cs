6b4a8428a2046670519c834cfcd9eb5fc90662c8
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-12 22:02:12 +0300

    Add files via upload

---
+++ AssociationBetweenClasses/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Inheritance
+{
+    // Inheritance used for 1. code reuse, 2. polymorphic behavior
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = new Text();
+            text.Width = 100;
+            text.Copy();
+
+        }
+    }
+}
