5a2321705f35247904ccd0ddfb08f2bd72d1ca78
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-13 23:17:30 +0300

    Add files via upload

---
+++ Inheritance/AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,16 @@
+﻿using Amazon;
+
+namespace AccessModifiers
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customer = new Customer();
+
+            // Cannot be accessed bc of internal class
+            //Amazon.RateCalculator calculator = new RateCalculator();
+        }
+    }
+}
---
+++ Inheritance/AccessModifiers/Amazon/Customer.cs
@@ -0,0 +1,19 @@
+﻿namespace Amazon
+{
+    public partial class Customer
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public void Promote()
+        {
+            var calculator = new RateCalculator();
+            var rating = calculator.Calculate();
+
+
+            Console.WriteLine("Promote Logic changed.");
+        }
+
+    }
+}
