f744977faf4901f5af33d7ae72f2273506fd5849
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-13 23:39:07 +0300

    Add files via upload

---
+++ Inheritance/ConstructorsAndInheritance/Constructors/Car.cs
@@ -0,0 +1,12 @@
+﻿namespace Constructors
+{
+    public class Car : Vehicle
+    {
+        public Car(string registrationNumber)
+            : base(registrationNumber)
+        {
+            Console.WriteLine("Car is being initialized. {0}", registrationNumber);
+        }
+    }
+
+}
---
+++ Inheritance/ConstructorsAndInheritance/Constructors/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Channels;
+
+namespace Constructors
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Constructor of the BASE CLASS is ALWAYS executed first.
+            var car = new Car("XYZ1234"); 
+        }
+    }
+
+}
