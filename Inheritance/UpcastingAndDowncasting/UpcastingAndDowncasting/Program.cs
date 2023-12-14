5bf103060165986a0eb9ff7780cc6e91a0db546e
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-14 11:14:56 +0300

    Add files via upload

---
+++ Inheritance/UpcastingAndDowncasting/UpcastingAndDowncasting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Casting
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape shape = new Text();
+            Text text = (Text) shape;
+        }
+    }
+}
