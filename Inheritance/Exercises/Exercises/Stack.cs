81d76e269cb0ec4c97c80813c12006052f752789
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-15 00:30:53 +0300

    Add files via upload

---
+++ Inheritance/Exercises/Exercises/Stack.cs
@@ -0,0 +1,47 @@
+﻿namespace Exercises
+{
+    public class Stack
+    {
+        public int stackSize { get { return stack.Count; } }
+
+        List<object> stack { get; set; }
+
+        public Stack() : this(0)
+        {
+
+        }
+
+        public Stack(int stackSize)
+        {
+            this.stack = new List<object>(stackSize);
+        }
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("The object is null, cannot be added.");
+            }
+            stack.Add(obj);
+        }
+
+        public object Pop()
+        {
+            if (stack.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+            var lastObject = stack[stack.Count - 1];
+            stack.Remove(lastObject);
+            return lastObject;
+        }
+
+        public void Clear()
+        {
+            while (stack.Count > 0)
+            {
+                stack.RemoveAt(0);
+            }
+        }
+    }
+}
