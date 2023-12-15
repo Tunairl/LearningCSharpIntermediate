78e887f2ea3d8df24afd79db04f3224247e51b48
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-15 23:59:39 +0300

    Add files via upload

---
+++ Polymorphism/MethodOverriding/MethodOverriding/Canvas.cs
@@ -0,0 +1,15 @@
+﻿namespace MethodOverriding
+{
+    public class Canvas
+    {
+        public void DrawShapes(List<Shape> shapes)
+        {
+            foreach (var shape in shapes)
+            {
+                shape.Draw();
+            }
+        }
+
+    }
+
+}
