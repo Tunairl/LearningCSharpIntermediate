d22cdcdcb87f750da43b879152d83082ac7f07cf
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-17 15:55:50 +0300

    Add files via upload

---
+++ Interfaces/Extensibility/Extensibility/DbMigrator.cs
@@ -0,0 +1,21 @@
+﻿namespace Extensibility
+{
+    public class DbMigrator
+    {
+        private readonly ILogger _logger;
+
+        public DbMigrator(ILogger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.LogInfo("Migration started at " + DateTime.Now);
+
+            // Details of migrating a db
+
+            _logger.LogInfo("Migration finished at " + DateTime.Now);
+
+        }
+    }
+}
---
+++ Interfaces/Extensibility/Extensibility/FileLogger.cs
@@ -0,0 +1,30 @@
+﻿namespace Extensibility
+{
+    public class FileLogger : ILogger
+    {
+        private readonly string _path;
+
+        public FileLogger(string path)
+        {
+            _path = path;
+        }
+        public void LogError(string message)
+        {
+            Log(message, "Error");
+        }
+
+        public void LogInfo(string message)
+        {
+            Log(message, "Info");
+        }
+
+        private void Log(string message, string messageType)
+        {
+            // using key word has a hidden exception handler
+            using (var streamWriter = new StreamWriter(_path, true))
+            {
+                streamWriter.WriteLine(messageType + ": " + message);
+            }
+        }
+    }
+}
