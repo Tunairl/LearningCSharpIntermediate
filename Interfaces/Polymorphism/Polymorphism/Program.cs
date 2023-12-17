6a27eaa007e122630416ec3536ffe5a5769d49eb
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-17 17:01:32 +0300

    Add files via upload

---
+++ Interfaces/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.Mail;
+
+namespace Polymorphism
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var encoder = new VideoEncoder();
+
+            encoder.RegisterNotificationChannel(new MailNotificationChannel());
+            encoder.RegisterNotificationChannel(new SmsNotificationChannel());
+
+            encoder.Encode(new Video());
+        }
+    }
+}
