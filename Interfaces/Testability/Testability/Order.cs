624834b8d5e576be72f6d916f27d98b97c674911
Tuna <32073253+Tunairl@users.noreply.github.com>
2023-12-17 12:14:42 +0300

    Add files via upload

---
+++ Interfaces/Testability/Testability/Order.cs
@@ -0,0 +1,25 @@
+﻿namespace Testability
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime DatePlaced { get; set; }
+        //public Shipment Shipment { get; set; }
+        public float TotalPrice { get; set; }   
+
+        public bool IsShipped
+        {
+            get { return Shipment != null; }
+        }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orderProccessor = new OrderProcessor(new ShippingCalculator);
+            var order = new Order {DatePlaced = DateTime.Now, TotalPrice = 100f};
+            orderProccessor.Process(order); 
+        }
+    }
+
+}
