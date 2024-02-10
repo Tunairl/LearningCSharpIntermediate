5f1e45c711f48344c8955e0c99a1cc54e34cce77
Tuna <32073253+Tunairl@users.noreply.github.com>
2024-02-10 17:32:25 +0300

    Add files via upload

---
+++ Classes/AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.SetBirthdate(new DateTime(2002, 9, 10));
+
+            Console.WriteLine(person.GetBirthdate());
+        }
+    }
+}
